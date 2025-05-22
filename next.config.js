62f751f3d6a668b0756abd3d37f23e70f91979dc
rugal2001 <brahim@lafactory.com>
2025-05-22 19:56:35 +0100

    merge

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  poweredByHeader: false,
+  typescript: {
+    // !! WARN !!
+    // For production, you should run type checking as part of your CI/CD process
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // For production, you should run linting as part of your CI/CD process
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
